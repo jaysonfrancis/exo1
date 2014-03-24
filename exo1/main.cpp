d063c32d137bd6dfd606ac7bee6b1147f56a26e6
jaysonfrancis <jaysonfrancis@gmail.com>
2014-03-24 15:38:41 -0700

    Initial commit from local git

---
+++ exo1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  exo1
+//
+//  Created by Jayson Francis on 3/24/14.
+//  Copyright (c) 2014 Jayson Francis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
